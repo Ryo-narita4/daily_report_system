205660cfaa2bef76668b4e7dd6ee4ae41fe9d1e8
ryo.narita <clowncrown24@gmail.com>
2020-11-23 10:21:24 +0900

    Add EncryptUtil.java

---
+++ src/controllers/employees/EmployeesCreateServlet.java
@@ -0,0 +1,54 @@
+package controllers.employees;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import utils.DBUtil;
+import utils.EncryptUtil;
+
+/**
+ * Servlet implementation class EmployeesCreateServlet
+ */
+@WebServlet("/employees/create")
+public class EmployeesCreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeesCreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())){
+            EntityManager em = DBUtil.createEntityManager();
+
+            Employee e = new Employee();
+
+            e.setCode(request.getParameter("code"));
+            e.setName(request.getParameter("name"));
+            e.setPassword(
+                    EncryptUtil.getPasswordEncrypt(
+                            request.getParameter("password"),
+                                (String)this.getServletContext().getAttribute("pepper")
+                            )
+                    );
+
+
+        }
+    }
+
+}
