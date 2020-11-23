05e78359831dbf7705b0d8af032653a90680ad62
ryo.narita <clowncrown24@gmail.com>
2020-11-23 11:00:15 +0900

    Add Employee create

--- src/controllers/employees/EmployeesCreateServlet.java
+++ src/controllers/employees/EmployeesCreateServlet.java
@@ -1,8 +1,11 @@
 package controllers.employees;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Employee;
+import models.validators.EmployeeValidator;
 import utils.DBUtil;
 import utils.EncryptUtil;
 
@@ -46,8 +50,32 @@ public class EmployeesCreateServlet extends HttpServlet {
                                 (String)this.getServletContext().getAttribute("pepper")
                             )
                     );
+            e.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
 
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            e.setCreated_at(currentTime);
+            e.setUpdated_at(currentTime);
+            e.setDelete_flag(0);
 
+            List<String> errors = EmployeeValidator.validate(e, true, true);
+            if(errors.size() > 0){
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("employee", e);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
+                rd.forward(request, response);
+            }else{
+                em.getTransaction().begin();
+                em.persist(e);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "登録が完了しました");
+                em.close();
+
+                response.sendRedirect(request.getContextPath() + "/employees/index");
+            }
         }
     }
 
