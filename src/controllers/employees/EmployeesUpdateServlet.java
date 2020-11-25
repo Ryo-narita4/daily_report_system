1258f969b353c2dc728bcdef875aa663f4490712
ryo.narita <clowncrown24@gmail.com>
2020-11-25 16:20:31 +0900

    Add Destroy / Update / login /logout

--- src/controllers/employees/EmployeesUpdateServlet.java
+++ src/controllers/employees/EmployeesUpdateServlet.java
@@ -66,7 +66,7 @@ public class EmployeesUpdateServlet extends HttpServlet {
             }
 
             e.setName(request.getParameter("name"));
-            e.setAdmin_flag(Integer.parseInt(request.getParameter("admi_flag")));
+            e.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
             e.setUpdated_at(new Timestamp(System.currentTimeMillis()));
             e.setDelete_flag(0);
 
